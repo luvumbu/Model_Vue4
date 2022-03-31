392c2d9e5c8b600eac23228a360cc87a6ab68752
luvumbu <luvumbu.n@gmail.com>
2022-03-31 12:09:52 +0200

    ok

---
+++ admin/index.php
@@ -0,0 +1,51 @@
+<iframe src="../index.php" frameborder="0"></iframe>
+
+
+
+<style>
+
+    iframe{
+        width:100%; 
+    }
+    body{
+        margin:0;
+        padding:0 ; 
+    }
+</style>
+
+<style>
+
+</style>
+
+
+
+<script>
+            var body = document.getElementsByTagName("body")[0]; 
+            body.setAttribute("class", "democlass"); 
+            body.setAttribute("style", "margin:0;padding:0"); 
+            body.setAttribute("onmousemove", "showCoords(event)"); 
+
+
+            body.setAttribute("onkeypress", "myFunction()"); 
+
+
+
+function myFunction(){
+    console.log("press ok") ; 
+}
+
+
+function showCoords(event) { 
+     
+console.log("..");
+ 
+
+
+}
+ 
+for(var i= 0 ; i<location.href.length;i++ ){
+
+    console.log(); 
+}
+             
+</script>
---
+++ model/class/php/showCoords.php
@@ -0,0 +1,69 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$servername = "localhost";
+$x_position =$_POST["x_position"] ;
+$y_position =$_POST["y_position"] ;
+
+$showCoords_ =$_POST["showCoords_"] ;
+
+$width_taille_page =$_POST["width_taille_page"] ;
+$height_taille_page =$_POST["height_taille_page"] ;
+
+
+
+
+
+
+$ajout = false;
+$filename = 'connexion.php';
+if (file_exists($filename)) {
+    
+    include("connexion.php") ;
+    $ajout = true;
+} else {
+    echo "Le fichier $filename n'existe pas.";
+    
+}
+
+
+
+
+echo $dbname ;
+echo $username ;
+echo $password ;
+echo "position en x ".$x_position ; 
+echo "position en y ".$y_position ; 
+$REMOTE_ADDR = $_SERVER['REMOTE_ADDR'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+if($showCoords_=="1"){
+  $sql = "INSERT INTO showCoords (adresse_ip,click,x_position,y_position,width_taille_page,height_taille_page)
+  VALUES ('$REMOTE_ADDR','false','$x_position','$y_position','$width_taille_page','$height_taille_page')";
+  }
+  else {
+    $sql = "INSERT INTO showCoords (adresse_ip,click,x_position,y_position,width_taille_page,height_taille_page)
+VALUES ('$REMOTE_ADDR','true','$x_position','$y_position','$width_taille_page','$height_taille_page')";
+  }
+  
+ 
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+
+
+
+
+?>
